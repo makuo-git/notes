a85643083a795993d4dd9639c837d3e0c97f2ebd
makuo-git <904578177@qq.com>
2021-08-11 14:47:04 +0800

    这是一些笔记

---
+++ first.bat
@@ -0,0 +1,14 @@
+
+@echo off
+set |findstr path
+net user
+echo '----------------      start learn dos----------'
+@REM 注释
+systeminfo
+set /a var=1+2
+echo %var%
+pause
+
+
+windos注册表机制  regeit  
+   相对与系统的数据库
